095a1ddfd0455123a9fb39dfcb1e1ac11a1beba7
lx <lx@DESKTOP-B5J0H2H>
2019-12-26 14:26:41 +0800

    数据结构：新增循环链表与其应用实例，约瑟夫问题

---
+++ src/com/yybt/datastructure/linkedlist/CylceLinkList.java
@@ -0,0 +1,117 @@
+package com.yybt.datastructure.linkedlist;
+
+/**
+ * 
+  * @ClassName: CylceLinkList
+  * @Description:  循环链表
+  * @author liuzehong
+ *
+ */
+public class CylceLinkList<T extends Comparable<T>> {
+	// 头结点
+	public Node<T> first;
+
+	public CylceLinkList() {
+		first=new Node<>();
+		first.data=null;
+		first.next=first;
+	}
+
+	/**
+	 * 插入一个结点
+	 */
+	public void insert(T value) {
+	
+		if (first.data == null) {
+			first=new Node<T>(value);
+			first.next = first;
+		} else {
+			Node<T> node = new Node<T>(value);
+			Node<T> temp = first;
+			while (temp.next!= first)
+				temp = temp.next;
+			temp.next = node;
+			node.next=first;
+
+		}
+	}
+
+	/**
+	 * 显示方法
+	 */
+	public void display() {
+		System.out.print(first.data + "\t");
+		Node<T> current = first;
+		while (current.next!= first) {
+			current = current.next;
+			System.out.print(current.data + "\t");
+		}
+		System.out.println();
+	}
+
+	/**
+	 * 查找方法
+	 */
+	public Node<T> find(T value) {
+		Node<T> current = first.next;
+		while (current.next != first) {
+			if (current.data.compareTo(value) == 0) {
+				break;
+			}
+			current = current.next;
+		}
+		return current;
+	}
+
+	
+	/**
+	 * 删除方法，根据数据域来进行删除
+	 */
+	public Node<T> delete(T value) {
+		Node<T> current = first;
+		Node<T> previous = first;
+		while(current.data .compareTo(value)!=0) {
+			if(current.next == null) {
+				return null;
+			}
+			previous = current;
+			current = current.next;
+		}
+		if(current == first) {
+			Node<T> node=first;
+			while(node.next!=first) {
+				node=node.next;
+			}
+			first = first.next;
+			node.next=first;
+		}else if(current.next==first) {
+			previous.next=first;
+		} else {
+			previous.next = current.next;
+		}
+		return current;
+		
+	}
+
+	/**
+	 * 判断是否为空
+	 */
+	public boolean isEmpty() {
+		return (first.next == first);
+	}
+
+	public static void main(String[] args) {
+		CylceLinkList<String> linklist = new CylceLinkList<>();
+		for (int i = 0; i < 5; i++) {
+			linklist.insert("张三" + i);
+		}
+		linklist.display();
+		linklist.delete("张三0");
+		System.out.println(linklist.find("张三4").next.data);
+		linklist.display();
+		linklist.display();
+		Node<String> node = linklist.find("张三3");
+		System.out.println(node.next.data);
+
+	}
+}
---
+++ src/com/yybt/example/other/ch02/Yuesefu.java
@@ -0,0 +1,44 @@
+/**
+ * @Title: Yuesefu.java
+ * @Package com.yybt.datastructure.linkedlist
+ */
+package com.yybt.example.other.ch02;
+
+import com.yybt.datastructure.linkedlist.CylceLinkList;
+import com.yybt.datastructure.linkedlist.Node;
+
+/**
+  * @ClassName: Yuesefu
+  * @Description: 循环链表的应用：约瑟夫问题
+  * @author liuzehong
+ **/
+public class Yuesefu {
+	
+
+	public  void deal(int total,int n) {
+		CylceLinkList<Integer> linkList=new CylceLinkList<>();
+		//初始化,编号1-total
+		for (int i = 1; i <= total; i++) {
+			linkList.insert(i);
+		}
+		Node<Integer> first=linkList.first;
+		Node<Integer> temp=first;
+		while (temp!=temp.next) {
+			for (int i = 1; i <n; i++) {
+				temp=temp.next;
+			}
+			//自杀：也就是从链表中删除
+			System.out.print(linkList.delete(temp.data).data+"\t");
+			temp=temp.next;
+		}
+		System.out.println();
+		System.out.print("存活下来的是：");linkList.display();
+		System.out.println("结束");
+		
+	}
+	
+	public static void main(String[] args) {
+		new Yuesefu().deal(41, 3);
+	}
+   
+}
